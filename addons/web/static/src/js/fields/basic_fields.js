51993a21038731c4ec259db39a1d7e68bf11249f
npandyangsa <33624353+npandyangsa@users.noreply.github.com>
2023-11-04 16:36:46 +0700

    UPD Numeric Field
    UPD Numeric Field trigger numeric keyboard

--- addons/web/static/src/js/fields/basic_fields.js
+++ addons/web/static/src/js/fields/basic_fields.js
@@ -464,6 +464,7 @@ var NumericField = InputField.extend({
         if (this.nodeOptions.step) {
             this.$input.attr({step: this.nodeOptions.step});
         }
+        this.$input.attr({inputmode: "numeric"});
         return result;
     },
 
